91d4f1df55f34827c8709f491519428314a3a316
liufubo <18380438251@163.com>
2016-12-27 14:26:37 +0800

    Initial Commit

---
+++ 新闻列表/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  新闻列表
+//
+//  Created by 刘富波 on 16/12/27.
+//  Copyright © 2016年 mac1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
